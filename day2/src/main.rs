b00279e804fa1acab0df09913b3f33de27167438
Dylan "smellyfis" Thies <dylan.thies@gmail.com>
2022-12-12 08:11:27 -0500

    push for pushing sake

---
+++ day2/src/main.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+#[derive(Debug)]
+struct HoHoError{}
+
+#[derive(PartialEq)]
+enum Choice{
+    Rock = 1,
+    Paper,
+    Scissors,
+}
+
+impl std::str::FromStr for Choice {
+    type Err = HoHoError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Choice::Rock),
+            "B" | "Y" => Ok(Choice::Paper),
+            "C" | "Z" => Ok(Choice::Scissors),
+            _ => Err(HoHoError{}),
+        }
+    }
+}
+
+impl Choice {
+    fn cmp(&self, opponent: &Self) -> i32 {
+        if self == opponent {
+            return 3;
+        }
+        if self.beats() == *opponent {
+            return 6;
+        }
+        0
+    }
+    fn beats (&self) -> Choice {
+        match self {
+            Choice::Rock => Choice::Scissors,
+            Choice::Paper => Choice::Rock,
+            Choice::Scissors => Choice::Paper,
+        }
+    }
+    fn loses(&self) -> Choice {
+        match self {
+            Choice::Rock => Choice::Paper,
+            Choice::Paper => Choice::Scissors,
+            Choice::Scissors => Choice::Rock,
+        }
+    }
+}
+
+struct Game{
+    pub opponent: Choice,
+    pub you: Choice,
+}
+
+impl std::str::FromStr for Game {
+    type Err = HoHoError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let str_split = s.split(" ").collect::<Vec<&str>>();
+        let opponent: Choice = str_split[0].parse()?;
+        // game1
+        //let you: Choice = str_split[1].parse()?;
+        let you = match str_split[1] {
+            "X" => opponent.beats(),
+            "Y" => str_split[0].parse()?,
+            "Z" => opponent.loses(),
+            _ => return Err(HoHoError{})
+        };
+        Ok(Game{opponent, you})
+    }
+}
+
+impl Game {
+    fn outcome (&self) -> i32 {
+        self.you.cmp(&self.opponent)
+    }
+
+    fn score(self) -> i32 {
+        let outcome = self.outcome();
+        (self.you as i32) + outcome
+    }
+}
+
+fn main() -> std::io::Result<()>{
+    //read in file
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+
+    let score: i32 = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<Game>().unwrap().score())
+        .sum();
+    println!("Puzzle 1: {score}");
+    Ok(())
+}
---
+++ day3/src/main.rs
@@ -0,0 +1,62 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn main() -> std::io::Result<()> {
+    //Read in file
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+
+    /*
+    let value = reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let (comp1, comp2) = line.split_at(line.len() / 2);
+            let duplicate = comp2.chars().find(|c| comp1.contains(*c)).unwrap();
+            match duplicate {
+                n @'a'..='z' => (n as i32) - ('a' as i32) + 1_i32,
+                n @ 'A'..='Z' => (n as i32) - ('A' as i32) + 27_i32,
+                _ => 0,
+            }
+        })
+        .sum::<i32>();
+    println!("Part 1: {value}");
+*/
+    //part 2
+    // fold the lines into groups of three
+    let value = reader.lines()
+        .fold(Vec::new(), |mut acc: Vec<Vec<String>>, line| {
+            if acc.is_empty() || acc.last().unwrap().len() == 3 {
+                acc.push(Vec::new())
+            }
+            acc.last_mut().unwrap().push(line.unwrap());
+            acc
+        })
+        .iter()
+        .map(|group| {
+            let (g1, g2, g3) = match group.as_slice() {
+                [g1, g2, g3] => (g1, g2, g3),
+                _ => panic!("not get here"),
+            };
+            match g1.chars() 
+                .fold(Vec::new(), |mut combo: Vec<char>, ch| {
+                    if g2.contains(ch) {
+                        combo.push(ch)
+                    }
+                    combo
+                })
+                .iter()
+                .find(|c| {
+                    g3.contains(**c)
+                }).unwrap(){
+                n @ 'a'..='z' => (*n as i32) - ('a' as i32) + 1_i32,
+                n @ 'A'..='Z' => (*n as i32) - ('A' as i32) + 27_i32,
+                _ => 0,
+            }
+        }).sum::<i32>();
+    println!("Part 2: {value}");
+    // find common letter in the groups
+    //   find common letters in the first 2 then find the common in the third
+    // sum the common letters
+    Ok(())
+}
