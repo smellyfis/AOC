8e5a3973074c3291849e38f3e22e02f196ab74d4
Dylan Thies <dylan.thies@gmail.com>
2023-09-01 21:15:22 -0400

    day 6 turned into testable and rfactored

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,40 +1,70 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::fs;
 
-fn main() -> std::io::Result<()> {
+use itertools::Itertools;
+
+fn work(input: &str, count: usize) -> String {
+    let array = input.as_bytes();
+    for x in count..array.len() {
+        if array
+            .iter()
+            .enumerate()
+            .filter_map(|(pos, val)| {
+                if pos >= x - count && pos < x {
+                    Some(val)
+                } else {
+                    None
+                }
+            })
+            .unique()
+            .count()
+            == count
+        {
+            return x.to_string();
+        }
+    }
+    panic!("stuff should have been gotten {input}")
+}
+
+fn part1(input: &str) -> String {
+    work(input, 4)
+}
+
+fn part2(input: &str) -> String {
+    work(input, 14)
+}
+
+fn main() -> () {
     //Read in file
-    let file = File::open("input")?;
-    let reader = BufReader::new(file);
+    let file = fs::read_to_string("input").unwrap();
 
-    reader.lines().enumerate().for_each(|(line_no, line)| {
-        let line = line.unwrap();
-        'checker: for i in 4..line.len() {
-            let window = line[(i - 4)..i].to_string();
-            let mut holder: HashMap<char, bool> = HashMap::new();
-            for c in window.chars() {
-                if holder.contains_key(&c) {
-                    continue 'checker;
-                }
-                holder.insert(c, true);
-            }
-            println!("Part 1: line: {line_no} marker at: {i}");
-            break;
-        }
-        'checker: for i in 14..line.len() {
-            let window = line[(i - 14)..i].to_string();
-            let mut holder: HashMap<char, bool> = HashMap::new();
-            for c in window.chars() {
-                if holder.contains_key(&c) {
-                    continue 'checker;
-                }
-                holder.insert(c, true);
-            }
-            println!("Part 2: line: {line_no} marker at: {i}");
-            break;
-        }
-    });
-    Ok(())
+    println!("Part 1: {}", part1(&file));
+    println!("Part 2: {}", part2(&file));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const INPUT: &[(&str, &str, &str)] = &[
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "7", "19"),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", "5", "23"),
+        ("nppdvjthqldpwncqszvftbrmjlhg", "6", "23"),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "10", "29"),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "11", "26"),
+    ];
+
+    #[test]
+    fn part1_works() {
+        INPUT
+            .iter()
+            .for_each(|(test, ans1, _)| assert_eq!(part1(*test), *ans1))
+    }
+
+    #[test]
+    fn part2_works() {
+        INPUT
+            .iter()
+            .for_each(|(test, _, ans2)| assert_eq!(part2(*test), *ans2))
+    }
 }
