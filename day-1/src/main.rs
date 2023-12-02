ac6662445d1a07905e182bf7ee064a101ac327ae
Dylan Thies <dylan.thies@gmail.com>
2023-12-01 22:27:47 -0500

    Day 1 done

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -5,7 +5,7 @@ use day_1::part2::part2;
 
 fn main() {
     let input = include_str!("./input.txt");
-    let (_,part1_result) = part1(input).unwrap();
+    let (_, part1_result) = part1(input).unwrap();
     println!("part 1: {part1_result}");
     let part2_result = part2(input);
     println!("part 2: {part2_result}");
--- day-1/src/part1.rs
+++ day-1/src/part1.rs
@@ -1,14 +1,32 @@
-use nom::{multi::{separated_list1}, self, character::complete::{alphanumeric1, newline}};
+use nom::{
+    self,
+    character::complete::{alphanumeric1, newline},
+    multi::separated_list1,
+};
 
-pub fn part1(input: &str) -> nom::IResult<&str,String> {
+pub fn part1(input: &str) -> nom::IResult<&str, String> {
     let (_, values) = parse_input(input)?;
     println!("{values:?}");
-    Ok(("", values.iter().map(|v| v.first().unwrap() * 10 + v.last().unwrap() ).sum::<u32>().to_string()))
+    Ok((
+        "",
+        values
+            .iter()
+            .map(|v| v.first().unwrap() * 10 + v.last().unwrap())
+            .sum::<u32>()
+            .to_string(),
+    ))
 }
 
 fn parse_input(input: &str) -> nom::IResult<&str, Vec<Vec<u32>>> {
     let (i, j) = separated_list1(newline, alphanumeric1)(input)?;
-    let res = j.iter().map(|v| v.chars().filter_map(|x| x.to_digit(10)).collect::<Vec<u32>>()).collect::<Vec<Vec<u32>>>();
+    let res = j
+        .iter()
+        .map(|v| {
+            v.chars()
+                .filter_map(|x| x.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
     Ok((i, res))
 }
 
@@ -25,4 +43,5 @@ treb7uchet";
     fn part1_works() {
         let (_, result) = part1(INPUT).unwrap();
         assert_eq!(result, "142".to_string());
-}}
+    }
+}
