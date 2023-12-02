3ac75e55bf6eca97ff7c7e27ec1271bed030eab1
Dylan Thies <dylan.thies@gmail.com>
2023-12-02 14:45:19 -0500

    day-3 prep

---
+++ day-3/src/part1.rs
@@ -0,0 +1,18 @@
+
+pub fn part1 (_input: &str) -> String {
+    "Not Finished".to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part1_works() {
+        let result = part1(INPUT);
+        assert_eq!(result, "Not Finished".to_string());
+    }
+}
+
---
+++ day-3/src/part2.rs
@@ -0,0 +1,18 @@
+
+pub fn part2 (_input: &str) -> String {
+    "Not Finished".to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part2_works() {
+        let result = part2(INPUT);
+        assert_eq!(result, "Not Finished".to_string());
+    }
+}
+
